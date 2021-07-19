7e2729ec0c655ba54f85276d7ccf830c303d687e
Rui Duraes <rui.duraes@jumia.com>
2021-07-19 15:43:20 +0100

    Remove commented code

--- Model/Jpay.php
+++ Model/Jpay.php
@@ -206,64 +206,6 @@ class Jpay extends \Magento\Payment\Model\Method\AbstractMethod {
 
       return $this;
   }
-//
-//    /* Extract the transaction and transaction data. */
-//    $parentTransactionData = $parentTransaction->getAdditionalInformation()['raw_details_info'];
-//
-//    /* Get the config values. */
-//    $apiKey = $this->config->getApiKey();
-//    $url = $this->config->getApiUrl();
-//    if (('' == $apiKey) || ('' == $url)) {
-//      /* Extract the message. */
-//      $message = __(' Refund failed: Incomplete or missing configuration');
-//      /* Log the error. */
-//      $this->log->error(__FUNCTION__ . ':' . $message);
-//
-//      throw new \Magento\Framework\Validator\Exception($message);
-//    }
-//
-//    /* Create the URL. */
-//    $url = $url . '/transaction/' . $parentTransactionData['transactionId'];
-//
-//    /* Create the DELETE data arguments. */
-//    $postData = 'amount=' . $amount . '&' . 'message=' . 'Refund for order ' . $parentTransactionData['orderId'];
-//    $this->log->info(__('Refund amount=%1 for transaction=%2 from order %3', $amount, $parentTransactionData['transactionId'], $parentTransactionData['orderId']));
-//
-//    /* Make the server request. */
-//    $ch = curl_init();
-//    curl_setopt($ch, CURLOPT_URL, $url);
-//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-//    curl_setopt($ch, CURLOPT_HTTPHEADER, ['accept: application/json', 'Authorization: ' . $apiKey]);
-//    curl_setopt($ch, CURLOPT_POST, strlen($postData));
-//    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-//
-//    /* Send the request. */
-//    $response = curl_exec($ch);
-//    curl_close($ch);
-//    /* Decode the response. */
-//    $response = json_decode($response);
-//
-//    /* Check if the decryption was successful, the response code is 200 and message is 'Success'. */
-//    if ((NULL !== $response) && (200 == $response->code) && ('Success' == $response->message)) {
-//      /* Updated the existing refund transaction with the received answer. */
-//      $payment->setTransactionId($response->data->transactionId);
-//      $payment->setTransactionAdditionalInfo( Transaction::RAW_DETAILS
-//                                            , [ 'orderId'               => $parentTransactionData['orderId']
-//                                              , 'refundedTransactionId' => $parentTransactionData['transactionId']
-//                                              , 'transactionId'         => $response->data->transactionId
-//                                              , 'amount'                => $amount]);
-//      $payment->setIsTransactionClosed(TRUE);
-//    } else {
-//      /* Extract the message. */
-//      $message = __(' Refund failed: Server returned error: %1', $response->code);
-//      /* Log the error. */
-//      $this->log->error(__FUNCTION__ . $message);
-//
-//      throw new \Magento\Framework\Validator\Exception($message);
-//    }
-//
-
 
   /**
    * Parent transaction id getter
