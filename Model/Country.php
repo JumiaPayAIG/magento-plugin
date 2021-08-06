c53e6304daec0bd6a558787bef28aec56c9c7088
Rui Duraes <rui.duraes@jumia.com>
2021-08-06 10:51:36 +0100

    Remove non available countries

--- Model/Country.php
+++ Model/Country.php
@@ -14,13 +14,13 @@ class Country implements ArrayInterface
         return [
             ['value' => '', 'label' => __('Please Select Country')],
             ['value' => 'eg', 'label' => __('Egypt')],
-            ['value' => 'gh', 'label' => __('Ghana')],
+/*            ['value' => 'gh', 'label' => __('Ghana')],
             ['value' => 'ci', 'label' => __('Ivory Coast')],
             ['value' => 'ke', 'label' => __('Kenya')],
             ['value' => 'ma', 'label' => __('Morocco')],
             ['value' => 'ng', 'label' => __('Nigeria')],
             ['value' => 'tn', 'label' => __('Tunisia')],
-            ['value' => 'ug', 'label' => __('Uganda')],
+            ['value' => 'ug', 'label' => __('Uganda')], */
         ];
     }
 }
